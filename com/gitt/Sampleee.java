e79f01c84bb2f8ce98fe847cd0efdc26b10e653b
rajansky <rajansky511@gmail.com>
2022-07-23 23:01:21 +0530

    new

---
+++ com/gitt/Sampleee.java
@@ -0,0 +1,12 @@
+package com.gitt;
+
+public class Sampleee {
+	public static void main(String[] args) {
+		System.out.println("welcome");
+		System.out.println("to");
+		System.out.println("git");
+		System.out.println("hub");
+	}
+	
+
+}
